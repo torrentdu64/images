66ca7f98b0d9d0c1850d37ec8c5721f9b68c3645
torrent benoit <torrentdu64@gmail.com>
2017-09-20 16:13:27 +0200

    render component

--- client/main.js
+++ client/main.js
@@ -1,1 +1,19 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const App = () => {
+  return (
+        <div>
+            React #2
+        </div>
+
+    );
+
+};
+
+Meteor.startup( () => {
+  ReactDOM.render(<App />, document.querySelector('.container'))
+
+});
+
