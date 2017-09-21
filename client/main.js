a30c451427884b0ddde4ae34e00ccf7aed30bfd8
torrent benoit <torrentdu64@gmail.com>
2017-09-21 17:30:43 +0200

    axios install

--- client/main.js
+++ client/main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import ImageList from './components/image_list';
 
 
@@ -15,6 +16,7 @@ const App = () => {
 
 Meteor.startup( () => {
   ReactDOM.render(<App />, document.querySelector('.container'))
-
+    axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+    .then( response => console.log(response))
 });
 
