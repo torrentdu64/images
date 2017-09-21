e6e8635d6cc4f3d4ef591f38a4f83adc714415de
torrent benoit <torrentdu64@gmail.com>
2017-09-21 15:59:00 +0200

    dummy data add to image detail

---
+++ client/components/image_detail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ImageDetail = () => {
+    return (
+        <div>
+            image detail
+        </div>
+      );
+};
+
+
+export default ImageDetail;
--- client/components/image_list.js
+++ client/components/image_list.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import ImageDetail from './image_detail';
+
+const IMAGEs = [
+    {title: 'pen', link: 'https://dummyimage.com/600x400/'},
+    {title: 'pine pee', link: 'https://dummyimage.com/600x400/'},
+    {title: 'piwi', link: 'https://dummyimage.com/600x400/'}
+];
 
 
 const ImageList = ( ) => {
   return (
     <ul>
-    <li>image list</li>
+    <ImageDetail />
     </ul>
     );
 };
