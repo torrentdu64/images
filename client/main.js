be82ee6c30ffa7937f96e813998b19ec497fa510
torrent benoit <torrentdu64@gmail.com>
2017-09-22 14:17:58 +0200

    change const to class App

--- client/main.js
+++ client/main.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import ImageList from './components/image_list';
 
 
-const App = () => {
+class App extends Component {
+  render() {
   return (
         <div>
             <ImageList />
@@ -12,6 +13,7 @@ const App = () => {
 
     );
 
+ }
 };
 
 Meteor.startup( () => {
